3f5cac92c18a49c1dc316976d3dc6f84884696be
segunanna <annasegun8@gmail.com>
2026-01-12 10:16:47 +0100

    initial commit

---
+++ alx-react-app/src/WelcomeMessage.jsx
@@ -0,0 +1,10 @@
+function WelcomeMessage() {
+    return (
+        <div>
+            <h1>"Hello Everyone, I am learnung React Alx!".</h1>
+            <p>"I am learning about JSX!".</p>
+        </div>
+    );
+}
+
+export default WelcomeMessage;
