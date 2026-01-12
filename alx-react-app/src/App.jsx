ae27094fcdb6572303180a852f66023d7bb86095
segunanna <annasegun8@gmail.com>
2026-01-12 17:45:47 +0100

    Initial Commit

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,11 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import WelcomeMessage from './components/WelcomeMessage'
+import WelcomeMessage from ".WelcomeMessage"
 import Header from "./Header"
 import MainContent from "./MainContent"
 import Footer from "./Footer"
-import UserProfile from "./components/UserProfile";
+import UserProfile from ".UserProfile";
+
+function App() {
+  return (
+    <div>
+      <WelcomeMessage />
+    </div>
+  );
+}
 
 function App() {
   return (
