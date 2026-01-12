01059d8d23a33b7ebb85e9de9e0f844f49df965f
segunanna <annasegun8@gmail.com>
2026-01-12 11:47:36 +0100

    Initial Commit

---
+++ alx-react-app/src/components/UserProfile.jsx
@@ -0,0 +1,9 @@
+const UserProfile = (props) => {
+   return (
+     <div>
+       <h2>Anna Segun</h2>
+       <p>Age: 23 years old</p>
+       <p>Bio: I am studying Software Engineering at ALX</p>
+     </div>
+   );
+ };
--- alx-react-app/src/components/WelcomeMessage.jsx
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -2,6 +2,7 @@ function WelcomeMessage() {
     return (
         <div>
             <h1>"Hello Everyone, I am learnung React Alx!".</h1>
+            <p>This is a simple JSX component.</p>
             <p>"I am learning about JSX!".</p>
         </div>
     );
